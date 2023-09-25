9f52b35821fc55c005a6cab659f3b4a755057791
Robert <rcastor0000@gmail.com>
2023-09-24 22:36:59 -0500

    Added masculine characters

--- script.js
+++ script.js
@@ -9,16 +9,18 @@ button1.addEventListener("click", async () => {
     const monstArr = []
     const response2 = response.data.data
     for (let i = 0; i < response2.length; i++) {
-            if (response2[i].name === 'Dark Magician Girl' || response2[i].name === 'Harpie Lady' || response2[i].name === 'Lovely Labrynth of the Silver Castle' || response2[i].name === 'Elemental HERO Burstinatrix' || response2[i].name === 'Amazoness Queen' || response2[i].name === 'Underworld Goddess of the Closed World' || response2[i].name === 'Isolde, Two Tales of the Noble Knights' || response2[i].name === 'Tour Guide From the Underworld' || response2[i].name === 'Vampire Vamp' || response2[i].name === 'Isolde, Belle of the Underworld' || response2[i].name === 'Maiden with Eyes of Blue' || response2[i].name === 'I:P Masquerena' || response2[i].name === 'Tour Guide From the Underworld') {
+            if (response2[i].name === 'Dark Magician Girl' || response2[i].name === 'Harpie Lady' || response2[i].name === 'Elemental HERO Burstinatrix' || response2[i].name === 'Amazoness Queen' || response2[i].name === 'Elemental HERO Neos' || response2[i].name === 'SPYRAL Super Agent' || response2[i].name === 'Dhampir Vampire Sheridan' || response2[i].name === 'Ancient Gear Golem') {
+                
+                
                 x = response2[i].card_images[0].image_url_cropped
                 monstArr.push(x)
 
                
 
                 // xName = response2[i]
-                
+                console.log(monstArr[8])
                 // console.log(xName.card_images[0].image_url_cropped)
-                monster.setAttribute('src', monstArr[11])
+                // monster.setAttribute('src', monstArr[8])
                 host.setAttribute('src', "kisspng-yugi-mutou-yami-yugi-yu-gi-oh-duel-links-yu-gi-oh-yu-5addf130a789d7.3981810715244946406863.png")
             }
 
